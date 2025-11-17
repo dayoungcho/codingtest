7ad85b73befe36edbd23f701447da7c77bc73c44
dayoungcho <89055775+dayoungcho@users.noreply.github.com>
2025-11-17 14:52:02 +0900

    [level 0] Title: 배열의 평균값, Time: 0.01 ms, Memory: 9.18 MB -BaekjoonHub

--- Python3/프로그래머스/0/120817. 배열의 평균값/배열의 평균값.py
+++ Python3/프로그래머스/0/120817. 배열의 평균값/배열의 평균값.py
@@ -1,4 +1,11 @@
-import numpy as np
+# import numpy as np
+# def solution(numbers):
+#     answer = np.mean(numbers)
+#     return answer
+
 def solution(numbers):
-    answer = np.mean(numbers)
+    answer = 0
+    for i in numbers:
+        answer += i
+    answer = answer / len(numbers)
     return answer
