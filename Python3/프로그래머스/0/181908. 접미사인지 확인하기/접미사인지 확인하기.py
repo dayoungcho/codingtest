b5eaa24de9fff5e5cb7eba31ee5a37bf55d1c024
dayoungcho <89055775+dayoungcho@users.noreply.github.com>
2025-11-18 14:15:09 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.00 ms, Memory: 9.22 MB -BaekjoonHub

--- Python3/프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.py
+++ Python3/프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.py
@@ -4,3 +4,10 @@ def solution(my_string, is_suffix):
         suffix.append(my_string[i:])
     answer = 1 if is_suffix in suffix else 0
     return answer
+
+
+# 다른풀이
+def solution(my_string, is_suffix):
+    if my_string[-len(is_suffix):]==is_suffix:
+        return 1
+    return 0
