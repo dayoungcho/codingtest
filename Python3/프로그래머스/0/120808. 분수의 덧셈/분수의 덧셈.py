c09cddb6ce1304889b4575d36cac71d8ad9098a4
dayoungcho <89055775+dayoungcho@users.noreply.github.com>
2025-11-18 15:55:00 +0900

    [level 0] Title: 분수의 덧셈, Time: 78.16 ms, Memory: 9.18 MB -BaekjoonHub

---
+++ Python3/프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.py
@@ -0,0 +1,13 @@
+def solution(numer1, denom1, numer2, denom2):
+    answer = []
+    a = numer1*denom2 + numer2*denom1
+    b = denom1*denom2
+
+    gcd = 1
+    for i in range(max(a,b),0,-1):
+        if a%i==0 and b%i==0:
+            gcd = i
+            break
+    answer = [a//gcd, b//gcd]
+        
+    return answer
