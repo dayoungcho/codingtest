e70b85910a8c13093d5f38cb3dff016a49967119
dayoungcho <89055775+dayoungcho@users.noreply.github.com>
2025-11-24 11:26:26 +0900

    [level 0] Title: 코드 처리하기, Time: 20.42 ms, Memory: 9.3 MB -BaekjoonHub

---
+++ Python3/프로그래머스/0/181932. 코드 처리하기/코드 처리하기.py
@@ -0,0 +1,21 @@
+def solution(code):
+    mode = 0
+    ret = ''
+    for i in range(len(code)):
+        if mode == 0:
+            if code[i]=="1":
+                mode = 1
+            else:
+                if i%2==0:
+                    ret += code[i]
+            continue
+        if mode == 1:
+            if code[i]=="1":
+                mode = 0
+            else:
+                if i%2==1:
+                    ret += code[i]
+            continue
+    if ret == '':
+        ret = "EMPTY"
+    return ret
