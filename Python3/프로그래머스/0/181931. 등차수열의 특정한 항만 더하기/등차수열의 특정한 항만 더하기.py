e5579852ff11bc9dd198a3c32fa729706e4bae31
dayoungcho <89055775+dayoungcho@users.noreply.github.com>
2025-11-24 13:40:06 +0900

    [level 0] Title: 등차수열의 특정한 항만 더하기, Time: 0.04 ms, Memory: 9.29 MB -BaekjoonHub

--- Python3/프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.py
+++ Python3/프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.py
@@ -7,3 +7,12 @@ def solution(a, d, included):
             d_num += i
     answer = a * a_num + d * d_num
     return answer
+
+
+# 똑똑하게 코딩한사람
+
+def solution(a, d, included):
+    answer = 0
+    for i in range(len(included)):
+        answer += (a + d * i) * int(included[i])
+    return answer
