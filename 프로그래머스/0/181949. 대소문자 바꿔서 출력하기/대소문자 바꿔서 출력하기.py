45b6d2b0cb09033e52714714ab77d06bcc9a276a
dayoungcho <89055775+dayoungcho@users.noreply.github.com>
2025-12-18 16:43:33 +0900

    Delete 프로그래머스 directory

--- 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.py
+++
@@ -1,8 +0,0 @@
-str = input()
-ans = ''
-for i in str:
-    if i.isupper():
-        ans += i.lower()
-    else:
-        ans += i.upper()
-print(ans)
--- 프로그래머스/1/301646. 특정 형질을 가지는 대장균 찾기/특정 형질을 가지는 대장균 찾기.sql
+++
@@ -1,1 +0,0 @@
-select count(*) as COUNT from ECOLI_DATA where GENOTYPE&2=0 and (GENOTYPE&1=1 or GENOTYPE&4>=4)
