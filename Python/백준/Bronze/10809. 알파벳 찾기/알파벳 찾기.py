823a5ed8dc29653e31cecabe2cba26907da0cc38
dayoungcho <89055775+dayoungcho@users.noreply.github.com>
2025-12-18 09:23:02 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
@@ -0,0 +1,8 @@
+S = input()
+alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+for i in alphabet:
+  if i in S:
+    print(S.index(i), end=' ')
+  else:
+    print(-1, end=' ')
