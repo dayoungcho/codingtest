3605e2f7851e12b52160db984cfe80f4d5586cf8
dayoungcho <89055775+dayoungcho@users.noreply.github.com>
2025-12-18 16:56:19 +0900

    [Bronze II] Title: Hashing, Time: 36 ms, Memory: 32412 KB, Score: 100 point -BaekjoonHub

--- Python/백준/Bronze/15829. Hashing/Hashing.py
+++ Python/백준/Bronze/15829. Hashing/Hashing.py
@@ -1,9 +1,10 @@
 l = int(input())
 s = input()
 r = 31
+m = 1234567891
 alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
 ans = 0
 for i in range(l):
   ans += (alphabet.index(s[i])+1)* r**i
-print(ans)
+print(ans%m)
