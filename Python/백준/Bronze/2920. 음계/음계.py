b7a57af48d4f6f9d47120b789329e1e35f0d1007
dayoungcho <89055775+dayoungcho@users.noreply.github.com>
2025-12-29 10:28:32 +0900

    [Bronze II] Title: 음계, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/2920. 음계/음계.py
@@ -0,0 +1,8 @@
+a,b,c,d,e,f,g,h = map(int, input().split())
+lst = [a,b,c,d,e,f,g,h]
+if lst == [1,2,3,4,5,6,7,8]:
+  print('ascending')
+elif lst == [8,7,6,5,4,3,2,1]:
+  print('descending')
+else:
+  print('mixed')
