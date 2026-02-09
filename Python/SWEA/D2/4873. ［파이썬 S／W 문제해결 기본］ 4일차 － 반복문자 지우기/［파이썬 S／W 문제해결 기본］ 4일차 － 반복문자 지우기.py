1f787fe37d166f7fb177cf3379d28f04f7ddcfca
dayoungcho <89055775+dayoungcho@users.noreply.github.com>
2026-02-09 14:45:32 +0900

    [D2] Title: [파이썬 S/W 문제해결 기본] 4일차 - 반복문자 지우기, Time: 72 ms, Memory: 52,608 KB -BaekjoonHub

---
+++ Python/SWEA/D2/4873. ［파이썬 S／W 문제해결 기본］ 4일차 － 반복문자 지우기/［파이썬 S／W 문제해결 기본］ 4일차 － 반복문자 지우기.py
@@ -0,0 +1,16 @@
+T = int(input())  # 테스트 케이스 수
+for _ in range(1, T+1):
+    chars = input()
+    stack = []
+    for char in chars:
+        # 스택 안에 아무것도 없으면 스택에 문자 집어넣기
+        if len(stack) == 0:
+            stack.append(char)
+        # 스택 맨 뒤의 값이 현재 문자와 같으면 꺼내기
+        elif stack[-1] == char:
+            out = stack.pop()
+        # 이외의 경우엔 스택에 문자 집어넣기
+        else:
+            stack.append(char)
+
+    print(f"#{_} {len(stack)}")
